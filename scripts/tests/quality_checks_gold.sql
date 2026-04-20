7a26ffed31cae9b03848087f4f4e694566d78c43
prernak199-create <prernak199@gmail.com>
2026-04-20 18:53:55 +0200

    Enhance quality checks in quality_checks_gold.sql
    Added checks for unique product keys and improved comments.

--- scripts/tests/quality_checks_gold.sql
+++ scripts/tests/quality_checks_gold.sql
@@ -1,4 +1,4 @@
-
+-
 --checking for Standardisation and consistency 
 select distinct gender from gold.dim_customers;
 ---------------------------------------------------------------------------------------------------------------
@@ -14,5 +14,32 @@ LEFT JOIN silver.erp_loc_a101 la
 on ci.cst_key=la.cid
 ORDER BY
 1,2;
+-------------------------------------------------------------------------------------------------------------------
+--check for unique product key in dimention product
+--Before creating 'gold.dim_product' and any other changes in column name or sequence
+--in the same query so it is written here
+--OLD QUERY before creating views 'gold.dim_product'
+select prd_key,COUNT(*)FROM (
+  select
+  pn.prd_id,
+  pn.cat_id,
+  pn.prd_key,
+  pn.prd_nm,
+  pn.prd_cost,
+  pn.prd_line,
+  pn.prd_start_dt,
+  pc.subcat,
+  pc.maintenance
+  from silver.crm-prd_info pn
+  LEFT JOIN silver.erp_px_cat_g1v2 pc
+  ON pn.cat_id = pc.id
+  where prd_end_dt is null ---Filter out all historical data
+  )t GROUP BY prd_key
+HAVING COUNT(*) > 1;
+----------------------------------------------------------------------------------------------------------------------------------------
+
+  
+
+
 
 
